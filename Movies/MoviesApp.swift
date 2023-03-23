e61e5c872b1e63628813b7973c33f293c44204af
praptioza <praptioza001@gmail.com>
2023-03-23 18:22:43 -0400

    Initial Commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by user232715 on 3/23/23.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
