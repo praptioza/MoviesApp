a59426d4adbe59c4f6b5dc920f677f8f2cbe3ea0
praptioza <praptioza001@gmail.com>
2023-03-25 01:45:40 -0400

    Updated Model

--- Movies/Movies.swift
+++ Movies/Movies.swift
@@ -16,14 +16,15 @@ struct Movie: Decodable{
     let posterPath: String?
     let backdropPath: String?
     let releaseDate: String?
-    let voteAvg: Double
+    let rating: Double?
+    let popularity: Double?
     
     enum CodingKeys: String, CodingKey{
-        case id, title, overview
+        case id, title, overview, popularity
         case posterPath = "poster_path"
         case backdropPath = "backdrop_path"
         case releaseDate = "release_date"
-        case voteAvg = "vote_average"
+        case rating = "vote_average"
     }
 }
 
@@ -31,36 +32,3 @@ struct MovieList: Decodable{
     let results: [Movie]
 }
 
-/*
-struct MovieDetail: Hashable, Decodable, Identifiable {
-    var id: Int
-    var overview: String
-    var releaseDate: String
-    var voteAverage: Double
-    var backdropPath: String?
-    var movies: [MovieInfo]
-    
-    enum CodingKeys: String, CodingKey {
-        case id
-        case overview
-        case releaseDate = "release_date"
-        case voteAverage = "vote_average"
-        case backdropPath = "backdrop_path"
-        case movies = "results"
-    }
-    
-    var posterURL: URL? {
-        guard let posterPath = movies.first?.posterPath else {
-            return nil
-        }
-        return URL(string: "https://image.tmdb.org/t/p/w342\(posterPath)")
-    }
-    
-    var backdropURL: URL? {
-        guard let backdropPath = backdropPath else {
-            return nil
-        }
-        return URL(string: "https://image.tmdb.org/t/p/w780\(backdropPath)")
-    }
-}
-*/
